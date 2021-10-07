31368dd9883f6d8e08399e2c6bc7bf4e10a1e940
hassan <hassanribery0335@gmail.com>
2021-10-07 22:25:39 +0500

    finished

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/profile', (req, res) => {
+  res.send('here is your profile')
+})
+router.post('/profile', (req, res) => {
+  res.send('profile created')
+})
+router.put('/profile', (req, res) => {
+  res.send('profile updated')
+})
+router.delete('/profile', (req, res) => {
+  res.send('profile deleted')
+})
+
 module.exports = router;
