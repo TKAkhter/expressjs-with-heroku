3ac0ddd6fea0fb8650c8d0b48b6617b962810eff
Talha Akhter <talhaakhter01@gmail.com>
2021-10-23 17:01:40 +0500

    view engine removed

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.use('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
-
-router.get('/api/v1/profile', (req, res) => {
-  res.send('here is your profile')
-})
-router.post('/api/v1/profile', (req, res) => {
-  res.send('profile created')
-})
-router.put('/api/v1/profile', (req, res) => {
-  res.send('profile updated')
-})
-router.delete('/api/v1/profile', (req, res) => {
-  res.send('profile deleted')
-})
 
 module.exports = router;
